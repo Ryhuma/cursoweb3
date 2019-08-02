a4831d6048513e65d3a043878582876ae8d15b58
Ryhuma <53542623+Ryhuma@users.noreply.github.com>
2019-08-02 16:11:26 -0300

    atualizacao

--- src/components/app.js
+++ src/components/app.js
@@ -5,11 +5,27 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {}
+        this.authFunc = this.authFunc.bind(this);
+    }
+    authFunc() {
+        const authData = { data: "Autenticar meu Site" };
+        if (WavesKeeper) {
+            WavesKeeper.auth( authData )
+            .then(auth => {
+                console.log( auth ); //Apresenta o resultado no console
+                /*...processing data */
+            }).catch(error => {
+                console.error( error ); // Apresenta o resultado no console
+                /*...processing errors */
+            })
+        } else {
+            alert("Para autenticar o WavesKeeper precisa estar instalado.");
+        }
     }
     render() {
         return (
             <div className="container">
-    		    <input className="btn btn-primary" type="submit" value="Alert" onClick={() => {alert("Alert button onClick");}}/>
+    		    <input className="btn btn-primary" type="submit" value="Auth" onClick={this.authFunc}/>
     	    </div>
         )
     }
